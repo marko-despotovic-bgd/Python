7dfb923a215cbd464916836ffacf5de8fab23905
Marko Despotovic <63671071+marko-despotovic-bgd@users.noreply.github.com>
2020-07-28 18:34:48 +0200

    Delete splitandjoin.py

--- splitandjoin.py
+++
@@ -1,8 +0,0 @@
-# You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
-
-
-line = input()
-line = line.split(" ")
-print(line)
-line = "-".join(line)
-print(line)
