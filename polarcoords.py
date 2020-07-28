0d23ccd70a237fd420b2f82d08ebe2d38e28febd
Marko Despotovic <63671071+marko-despotovic-bgd@users.noreply.github.com>
2020-07-28 18:35:15 +0200

    Delete polarcoords.py

--- polarcoords.py
+++
@@ -1,8 +0,0 @@
-# You are given a complex . Your task is to convert it to polar coordinates.
-# https://www.hackerrank.com/challenges/polar-coordinates/problem
-from cmath import phase
-
-z = input()
-
-print(abs(complex(z)))
-print(phase(complex(z)))
