e420ac980256a13f241c1ff62f63f798425656cd
Marko Despotovic <63671071+marko-despotovic-bgd@users.noreply.github.com>
2020-07-28 18:34:42 +0200

    Delete swapcase.py

--- swapcase.py
+++
@@ -1,6 +0,0 @@
-# You are given a string and your task is to swap cases. In other words,
-# convert all lowercase letters to uppercase letters and vice versa.
-
-s = input()
-result = s.swapcase()
-print(result)
