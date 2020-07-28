a8481f0b03e0438729f3290b07a9e52aff1fbc59
Marko Despotovic <63671071+marko-despotovic-bgd@users.noreply.github.com>
2020-07-28 18:33:09 +0200

    Delete modpower.py

--- modpower.py
+++
@@ -1,8 +0,0 @@
-# You are given three integers: , , and , respectively. Print two lines.
-# The first line should print the result of pow(a,b).
-# The second line should print the result of pow(a,b,m).
-
-
-a, b, m = (int(input()) for _ in range(3))
-print(pow(a, b))
-print(pow(a, b, m))
